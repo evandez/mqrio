0b1e5bb8482246969b7a05ec37edb5cd3d2fca2b
Evan Hernandez <evanfredhernandez@gmail.com>
2017-04-08 18:02:32 -0500

    Remove old stuff.

--- dqn.py
+++
@@ -1,8 +0,0 @@
-"""Wrapper around tensorflow for the nonlinear Q approximator."""
-
-class DQN(object):
-    """Defines an interface for interacting with the nonlinear Q approximator.
-    Uses TensorFlow for all CNN logic.
-    """
-    def __init__(self):
-        print 'Deep Q Network created successfully!'
