Fix incorrect gradient calculation. Restructure a little.

learner/qlearn.py
@@ -15,8 +15,7 @@ class DeepQLearner(object):
             actions: List of viable actions learner can make. (Must be PyGame constants.)
         """
         self.actions = actions
-        self.target_net = QNet(len(actions))
-        self.training_net = self.target_net.deep_copy()
+        self.net = QNet(len(actions))
         self.exploration_rate = cg.EXPLORATION_START_RATE
         self.iteration = -1
 
@@ -79,10 +78,10 @@ class DeepQLearner(object):
         """Returns true if a random action should be taken, false otherwise.
         Decays the exploration rate if the final exploration frame has not been reached.
         """
-        if self.iteration <= cg.FINAL_EXPLORATION_FRAME:
+        if len(self.transitions) <= cg.FINAL_EXPLORATION_FRAME:
             self.exploration_rate -= (
                 float(cg.EXPLORATION_START_RATE - cg.EXPLORATION_END_RATE)
-                / cg.FINAL_EXPLORATION_FRAME)
+                / (cg.FINAL_EXPLORATION_FRAME - cg.REPLAY_START_SIZE))
         return random.random() < self.exploration_rate
 
     def best_action(self, frame):
@@ -91,14 +90,14 @@ class DeepQLearner(object):
         Args:
             frame: The current frame.
         """
-        return self.actions[np.argmax(self.target_net.compute_q(frame))]
+        return self.actions[np.argmax(self.net.compute_q(frame))]
 
     def random_action(self):
         """Returns a random action to perform."""
         return self.actions[int(random.random() * len(self.actions))]
 
     def compute_target_reward(self, trans):
-        """Computes the target reward for the given transition using the target Q-network.
+        """Computes the target reward for the given transition.
 
         Args:
             trans: The transition for which to compute the target reward.
@@ -107,9 +106,9 @@ class DeepQLearner(object):
             The target reward.
         """
         target_reward = trans['reward']
-        if not trans['terminal'] and trans['time'] < len(self.transitions) - 1:
+        if not trans['terminal'] and trans['time'] != self.transitions[-1]['time']:
             next_input = self.transitions[trans['time']+1]['input']
-            target_reward += cg.DISCOUNT * self.target_net.compute_q(next_input)[trans['action']]
+            target_reward += cg.DISCOUNT * np.amax(self.net.compute_q(next_input))
         return target_reward
 
     def step(self, frame, reward, terminal):
@@ -145,13 +144,10 @@ class DeepQLearner(object):
 
         # Update network from the previous action.
         minibatch = random.sample(self.transitions, cg.BATCH_SIZE)
-        batch_input = [trans['input'] for trans in minibatch]
-        batch_target = [self.compute_target_reward(trans) for trans in minibatch]
-        self.training_net.update(batch_input, batch_target)
-
-        # If it's time to update the target network, do so.
-        if self.iteration % cg.UPDATE_FREQUENCY == 0:
-            self.target_net = self.training_net.deep_copy()
+        batch_frames = [trans['input'] for trans in minibatch]
+        batch_actions = [trans['action'] for trans in minibatch]
+        batch_targets = [self.compute_target_reward(trans) for trans in minibatch]
+        self.net.update(batch_frames, batch_actions, batch_targets)
 
         # Select the next action.
         proc_frame = self.preprocess(frame)

learner/qnet.py
@@ -1,5 +1,6 @@
 """Thin wrapper around TensorFlow logic."""
 import learner.config as cg
+import learner.graph as graph
 import tensorflow as tf
 
 class QNet(object):
@@ -15,116 +16,21 @@ class QNet(object):
         Args:
             output_width: The number of output units.
         """
-        self.output_width = output_width # Hold onto this value for copying.
+        self.graph_in, self.graph_out = graph.construct_graph(output_width)
 
-        self.graph = tf.Graph()
-        with self.graph.as_default():
-            self.graph_in = tf.placeholder(tf.float32, shape=[None, 84, 84, 4])
+        self.target_reward = tf.placeholder(tf.float32)
+        self.action_idxs = tf.placeholder(tf.int32)
+        actual_reward = tf.gather_nd(self.graph_out, self.action_idxs)
+        loss = tf.nn.l2_loss(self.target_reward - actual_reward)
+        clipped_loss = tf.clip_by_value(loss, -1, 1)
+        self.optimizer = tf.train.AdamOptimizer(cg.LEARNING_RATE).minimize(clipped_loss)
 
-            w_conv1 = QNet._weight_variable([8, 8, 4, 32])
-            b_conv1 = QNet._bias_variable([32])
-            conv_layer1 = tf.nn.relu(QNet._conv2d(self.graph_in, w_conv1, 4) + b_conv1)
-
-            w_conv2 = QNet._weight_variable([4, 4, 32, 64])
-            b_conv2 = QNet._bias_variable([64])
-            conv_layer2 = tf.nn.relu(QNet._conv2d(conv_layer1, w_conv2, 2) + b_conv2)
-
-            w_conv3 = QNet._weight_variable([3, 3, 64, 64])
-            b_conv3 = QNet._bias_variable([64])
-            conv_layer3 = tf.nn.relu(QNet._conv2d(conv_layer2, w_conv3, 1) + b_conv3)
-
-            conv_layer3_flat = tf.reshape(conv_layer3, [-1, 7744])
-            w_fc1 = QNet._weight_variable([7744, 512])
-            b_fc1 = QNet._bias_variable([512])
-            fc_layer1 = tf.nn.relu(tf.matmul(conv_layer3_flat, w_fc1) + b_fc1)
-
-            w_fc2 = QNet._weight_variable([512, output_width])
-            b_fc2 = QNet._bias_variable([output_width])
-            self.graph_out = tf.nn.relu(tf.matmul(fc_layer1, w_fc2) + b_fc2)
-
-            self.target_reward = tf.placeholder(tf.float32)
-            loss = tf.nn.l2_loss(self.target_reward - tf.reduce_max(self.graph_out))
-            clipped_loss = tf.clip_by_value(loss, -1, 1)
-            self.optimizer = tf.train.RMSPropOptimizer(
-                cg.LEARNING_RATE, momentum=cg.MOMENTUM).minimize(clipped_loss)
-
-            self.sess = tf.Session(graph=self.graph)
-            self.sess.run(tf.global_variables_initializer())
+        self.sess = tf.Session()
+        self.sess.run(tf.global_variables_initializer())
 
     def __del__(self):
         """Closes the TensorFlow session, freeing resources."""
         self.sess.close()
-
-    @staticmethod
-    def _conv2d(data, weights, stride):
-        """Returns a TensforFlow 2D convolutional layer.
-
-        Args:
-            data: The input tensor to the convolutional layer.
-            weights: The convolutional weights for this layer.
-            stride: The x and y stride for the convolution.
-
-        Returns:
-            The TensorFlow convolutional layer.
-        """
-        return tf.nn.conv2d(data, weights, strides=[1, stride, stride, 1], padding='SAME')
-
-    @staticmethod
-    def _weight_variable(shape):
-        """Returns a TensforFlow weight variable.
-
-        Args:
-            shape: The shape of the weight variable.
-
-        Returns:
-            A TensorFlow weight variable of the given size.
-        """
-        return tf.Variable(tf.truncated_normal(shape, stddev=0.1))
-
-    @staticmethod
-    def _bias_variable(shape):
-        """Returns a TensforFlow 2D bias variable.
-
-        Args:
-            shape: The shape of the bias variable.
-
-        Returns:
-            A TensorFlow bias variable of the specified shape.
-        """
-        return tf.Variable(tf.constant(0.1, shape=shape))
-
-    def save(self, filename):
-        """Saves this network's current graph variables.
-
-        Args:
-            filename: The name of the file to save the variables to.
-        """
-        with self.graph.as_default():
-            tf.train.Saver().save(self.sess, filename)
-
-    def load(self, filename):
-        """Loads the network variables from a checkpoint file.
-
-        Args:
-            filename: The name of the checkpoint file.
-        """
-        with self.graph.as_default():
-            tf.train.Saver().restore(self.sess, filename)
-
-    def deep_copy(self):
-        """Writes the current network to a checkpoint, then loads a new network
-        of the same structure from that checkpoint. Effectively creates a deep
-        copy of this network.
-
-        Returns:
-            A Q-net with the same structure and parameters as this network, but under
-            a different TensorFlow session.
-        """
-        filename = '/tmp/transfer.chk'
-        copy = QNet(self.output_width)
-        self.save(filename)
-        copy.load(filename)
-        return copy
 
     def compute_q(self, net_in):
         """Forward-propagates the given input and returns the array of outputs.
@@ -135,15 +41,21 @@ class QNet(object):
         Returns:
             The array of network outputs.
         """
-        return self.sess.run(self.graph_out, feed_dict={self.graph_in:[net_in]})[0]
+        return self.sess.run(self.graph_out, feed_dict={self.graph_in:[net_in]})
 
-    def update(self, batch_input, batch_target):
+    def update(self, batch_frames, batch_actions, batch_targets):
         """Updates the network with the given batch input/target values using RMSProp.
 
         Args:
-            batch_input: Set of Nx84x84x4 network inputs, where N is the batch size.
+            batch_frames: Set of Nx84x84x4 network inputs, where N is the batch size.
+            batch_actions: Set of N action indices, representing action taken at each state.
             batch_target: Corresponding target Q values for each input.
         """
+        # Note: Action indicies must actually be tuples since graph_out is a 2D tensor.
+        # To prevent tight coupling, modify the batch_actions list here.
         self.sess.run(
             self.optimizer,
-            feed_dict={self.graph_in:batch_input, self.target_reward:batch_target})
+            feed_dict={
+                self.graph_in:batch_frames,
+                self.action_idxs:[tup for tup in enumerate(batch_actions)],
+                self.target_reward:batch_targets})