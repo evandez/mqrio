e52115e281b137815edf2529a6d3c165be074b39
dliangsta <davidliangx27@gmail.com>
2017-05-03 09:56:05 -0500

    param

--- learner/config.py
+++ learner/config.py
@@ -36,10 +36,10 @@ STATE_FRAMES = 4
 
 # Number of iterations for which we take random actions, to build a foundation
 # for our state-action-reward memory.
-REPLAY_START_SIZE = 500
+REPLAY_START_SIZE = 50000
 
 # Maximum size of the replay memory.
-REPLAY_MEMORY_SIZE = 1000
+REPLAY_MEMORY_SIZE = 100000
 
 # How often the Q-learner should log its state.
 LOGGING_FREQUENCY = 100
