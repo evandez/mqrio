fab83cc1b387b66f8fa5433078afead5144e8530
dliangsta <davidliangx27@gmail.com>
2017-05-07 12:01:52 -0500

    made pong smaller, faster

--- augmented_pong_player.py
+++ augmented_pong_player.py
@@ -1,18 +1,18 @@
 """Simple Pong Player for testing Deep Q logic."""
-import pygame.constants as pgc
+from pygame.constants import K_DOWN, K_UP, K_UNKNOWN
 from PyGamePlayer.pygame_player import PyGamePlayer
-from learner.config import *
 from learner.qlearn import DeepQLearner
+from learner.config import LOG_FREQUENCY
 
 # Possible actions for Pong. Last one is equivalent to "do nothing."
-ACTIONS = [pgc.K_DOWN, pgc.K_UP, pgc.K_UNKNOWN]
+ACTIONS = [K_DOWN, K_UNKNOWN, K_UP]
 
 # Either 'HITS' or 'SCORES'.
 SCORING_FUNCTION = 'HITS'
 
 class AugmentedPongPlayer(PyGamePlayer):
     """Implementation of PyGamePlayer for Pong."""
-    def __init__(self, force_game_fps=10, run_real_time=False):
+    def __init__(self, force_game_fps=8, run_real_time=False):
         """
         Example class for playing Pong
         """
@@ -21,14 +21,16 @@ class AugmentedPongPlayer(PyGamePlayer):
             run_real_time=run_real_time)
         self.last_bar1_score = 0.0
         self.last_bar2_score = 0.0
-
-        self.last_bar1_hit_count = 0.
-        self.last_bar2_hit_count = 0.
+        self.last_hit_count = 0
+        self.last_miss_count = 0
+        self.starting_hit_count = 0
+        self.starting_miss_count = 0
+        self.score_ratio = None
 
         self.dql = DeepQLearner(ACTIONS, save=True)
 
     def get_keys_pressed(self, screen_array, feedback, terminal):
-        return self.dql.step(screen_array, feedback, terminal)
+        return self.dql.step(screen_array, feedback, terminal, self.score_ratio)
 
     def get_feedback(self):
         # import must be done here because otherwise importing would cause the game to start playing
@@ -41,19 +43,24 @@ class AugmentedPongPlayer(PyGamePlayer):
             self.last_bar2_score = bar2_score
 
         elif SCORING_FUNCTION == 'HITS':
-            from games.augmented_pong import bar1_hit_count, bar2_hit_count
+            from games.pong import hit_count, miss_count
 
-            score_change = ((bar1_hit_count - self.last_bar1_hit_count)
-                            - (bar2_hit_count - self.last_bar2_hit_count))
+            # get the difference in score between this and the last run
+            score_change = (hit_count - self.last_hit_count) - (miss_count - self.last_miss_count)
+            self.last_miss_count = miss_count
+            self.last_hit_count = hit_count
 
-            self.last_bar1_hit_count = bar1_hit_count
-            self.last_bar2_hit_count = bar2_hit_count
+            if self.last_miss_count % LOG_FREQUENCY == 0:
+                self.starting_miss_count = self.last_miss_count
+                self.starting_hit_count = self.last_hit_count
+            
+            self.score_ratio = float((hit_count - self.starting_hit_count) / (miss_count - self.starting_miss_count + 1))
 
         return float(score_change), score_change != 0
 
     def start(self):
         super(AugmentedPongPlayer, self).start()
-        import games.augmented_pong
+        import games.pong
 
 if __name__ == '__main__':
     AugmentedPongPlayer().start()
